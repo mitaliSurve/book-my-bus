20e25c59ad6f284e21c04f86c3bad93aad258558
mitali28 <mitalisurve2@gmail.com>
2021-03-31 17:48:49 +0530

    fix seat border issue

--- src/pages/customer/Customer.js
+++ src/pages/customer/Customer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import cx from 'classnames';
 import Dialog from "../../components/dialogMenu/Dialog";
 import style from "../customer/customer.module.css";
 
@@ -9,7 +9,7 @@ const Customer = ({ onClose }) => {
     <Dialog header="Customer Form" onClose={onClose} action="book">
       <form>
         <div className={style.root}>
-          <label htmlFor="name" className={style.label}>
+          <label htmlFor="name" className={cx(style.label, style.label_color)}>
             Name
           </label>
           <input
@@ -20,7 +20,7 @@ const Customer = ({ onClose }) => {
             className={style.input}
           />
 
-          <label htmlFor="seatNo" className={style.label}>
+          <label htmlFor="seatNo" className={cx(style.label, style.label_color)}>
             Seat no
           </label>
           <input
@@ -31,7 +31,7 @@ const Customer = ({ onClose }) => {
             className={style.input}
           />
 
-          <label htmlFor="age" className={style.label}>
+          <label htmlFor="age" className={cx(style.label, style.label_color)}>
             Age
           </label>
           <input
@@ -42,7 +42,7 @@ const Customer = ({ onClose }) => {
             className={style.input}
           />
 
-          <label htmlFor="price" className={style.label}>
+          <label htmlFor="price" className={cx(style.label, style.label_color)}>
             Price
           </label>
           <input
